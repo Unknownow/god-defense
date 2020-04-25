b57e037b066eb6b4ca60843634a91ebe8db5fa68
BTHieu <20161498@student.hust.edu.vn>
2020-04-26 01:02:58 +0700

    Cài test 4 enemies

---
+++ Assets/Animations/EnemyHumanController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHumanController : MonoBehaviour
+{
+
+    CharacterController controller;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.G)) {
+            animator.SetBool("runFlag", true);
+        } else if (Input.GetKey (KeyCode.H)) {
+            animator.SetBool("runFlag", false);
+        }
+    }
+}
