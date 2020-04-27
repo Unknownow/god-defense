a855ac4bf9fd3a1c39946636d906fb16fb03d324
Cường Bùi <cuong.bpt82@gmail.com>
2020-04-27 23:00:38 +0700

    added navmesh

---
+++ Assets/SimpleMovementController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleMovementController : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Camera cam;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
