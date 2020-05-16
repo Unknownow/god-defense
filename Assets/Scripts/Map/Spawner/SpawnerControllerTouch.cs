9d18b8d84ff35e9f906edddeca1f60b9519eb19c
Cường Bùi <cuong.bpt82@gmail.com>
2020-05-16 21:49:50 +0700

    Remove vuforia, re-arrange assets folder

--- Assets/Scripts/Map/Spawner/SpawnerControllerTouch.cs
+++ Assets/Scripts/Map/Spawner/SpawnerControllerTouch.cs
@@ -1,65 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class SpawnerControllerTouch : MonoBehaviour
-{
-    [SerializeField]
-    private Transform _enemyParent;
-    private SpawnerProperties _spawnerProperties;
-    private float maxTouchTime = 0.5f;
-    private float lastTouchTime;
-    private int tapCount;
+// public class SpawnerControllerTouch : MonoBehaviour
+// {
+//     [SerializeField]
+//     private Transform _enemyParent;
+//     private SpawnerProperties _spawnerProperties;
+//     private float maxTouchTime = 0.5f;
+//     private float lastTouchTime;
+//     private int tapCount;
 
-    private void Start()
-    {
-        _spawnerProperties = gameObject.GetComponent<SpawnerProperties>();
-    }
-    private void Update()
-    {
-        if( Input.touchCount == 1  && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
-            if (tapCount > 0)
-            {
-                tapCount++;
-            }
-            else
-            {
-                tapCount = 1;
-                lastTouchTime = Time.time;
-            }
-        }
-        if (Time.time - lastTouchTime > maxTouchTime)
-        {
-            if (tapCount == 1)
-            {
-                SpawnEnemy(EnemyType.Runner);
-            }
-            if (tapCount > 1)
-            {
-                SpawnEnemy(EnemyType.Heavier);
-            }
-            tapCount = 0;
-        }
-    }
+//     private void Start()
+//     {
+//         _spawnerProperties = gameObject.GetComponent<SpawnerProperties>();
+//     }
+//     private void Update()
+//     {
+//         if( Input.touchCount == 1  && Input.GetTouch(0).phase == TouchPhase.Began)
+//         {
+//             if (tapCount > 0)
+//             {
+//                 tapCount++;
+//             }
+//             else
+//             {
+//                 tapCount = 1;
+//                 lastTouchTime = Time.time;
+//             }
+//         }
+//         if (Time.time - lastTouchTime > maxTouchTime)
+//         {
+//             if (tapCount == 1)
+//             {
+//                 SpawnEnemy(EnemyType.Runner);
+//             }
+//             if (tapCount > 1)
+//             {
+//                 SpawnEnemy(EnemyType.Heavier);
+//             }
+//             tapCount = 0;
+//         }
+//     }
 
-    public GameObject SpawnEnemy(EnemyType type)
-    {
-        if (_spawnerProperties.SpawnableTypes.IndexOf(type) == -1)
-            return null;
-        GameObject enemy;
-        switch (type)
-        {
-            case EnemyType.Runner:
-                enemy = EnemyFactory.SpawnRunner(transform.position, _spawnerProperties.SpawnerDirection, _enemyParent, _spawnerProperties.LaneIndex);
-                enemy.GetComponent<EnemyMovement>().SetDestination(_spawnerProperties.Target);
-                return enemy;
-            case EnemyType.Heavier:
-                enemy = EnemyFactory.SpawnHeavier(transform.position, _spawnerProperties.SpawnerDirection, _enemyParent, _spawnerProperties.LaneIndex);
-                enemy.GetComponent<EnemyMovement>().SetDestination(_spawnerProperties.Target);
-                return enemy;
-            default:
-                return null;
-        }
-    }
-}
+//     public GameObject SpawnEnemy(EnemyType type)
+//     {
+//         if (_spawnerProperties.SpawnableTypes.IndexOf(type) == -1)
+//             return null;
+//         GameObject enemy;
+//         switch (type)
+//         {
+//             case EnemyType.Runner:
+//                 enemy = EnemyFactory.SpawnRunner(transform.position, _spawnerProperties.SpawnerDirection, _enemyParent, _spawnerProperties.LaneIndex);
+//                 enemy.GetComponent<EnemyMovement>().SetDestination(_spawnerProperties.Target);
+//                 return enemy;
+//             case EnemyType.Heavier:
+//                 enemy = EnemyFactory.SpawnHeavier(transform.position, _spawnerProperties.SpawnerDirection, _enemyParent, _spawnerProperties.LaneIndex);
+//                 enemy.GetComponent<EnemyMovement>().SetDestination(_spawnerProperties.Target);
+//                 return enemy;
+//             default:
+//                 return null;
+//         }
+//     }
+// }
