2d617e907c8f4bd189d1153c4a2bd56d4e4bf7ed
Cường Bùi <cuong.bpt82@gmail.com>
2020-05-19 23:33:57 +0700

    import xong scene của hiếu

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform _playerParent;
+    protected PlayerProperties _playerProperties;
+
+    private void Start()
+    {
+        _playerParent = gameObject.transform;
+        _playerProperties = gameObject.GetComponent<PlayerProperties>();
+    }
+
+    private void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        {
+            SpawnBullet();
+        }
+    }
+
+    public void SpawnBullet()
+    {
+        GameObject bullet;
+        bullet = BulletFactory.SpawnBullet(transform.position, transform.forward, _playerParent);
+        BulletMovement _bulletMovement = bullet.GetComponent<BulletMovement>();
+
+        _bulletMovement.ForwardShoot();
+    }
+}
---
+++ Assets/Scripts/Player/PlayerProperties.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProperties : MonoBehaviour
+{
+    [SerializeField]
+    private string _name;
+    public string Name
+    {
+        get
+        {
+            return this._name;
+        }
+    }
+
+    [SerializeField]
+    private Direction _playerDirection;
+    public Vector3 PlayerDirection
+    {
+        get
+        {
+            Vector3 dir = Vector3.zero;
+            switch (this._playerDirection)
+            {
+                case Direction.North:
+                    dir = -Vector3.forward;
+                    break;
+                case Direction.South:
+                    dir = Vector3.forward;
+                    break;
+                case Direction.East:
+                    dir = Vector3.right;
+                    break;
+                case Direction.West:
+                    dir = -Vector3.right;
+                    break;
+            }
+            return dir;
+        }
+    }
+}
