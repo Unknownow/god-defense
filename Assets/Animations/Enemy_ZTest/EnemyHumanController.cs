4e0ac56f7bcd4cd397756e105c61d289f07b45d2
BTHieu <20161498@student.hust.edu.vn>
2020-05-24 17:00:54 +0700

    Thêm model + animation cho 3 loại enemies

---
+++ Assets/Animations/Enemy_ZTest/EnemyHumanController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHumanController : MonoBehaviour
+{
+
+    CharacterController controller;
+    Animator animator;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.transform.CompareTag("Bullet"))
+        {
+            animator.SetBool("runFlag", true);
+        }
+    }
+}
