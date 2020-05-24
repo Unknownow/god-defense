b2b1ebabe6d517348a031eafb942090b7ea70f10
BTHieu <20161498@student.hust.edu.vn>
2020-05-24 20:00:02 +0700

    Đổi enemy 1 và 3 (vẫn có 2 con cũ đấy). Thêm Collider vs 1 số thứ.

---
+++ Assets/Animations/Enemy_First/Enemy_First_Controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_First_Controller : MonoBehaviour
+{
+    CharacterController controller;
+    Animator animator;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.transform.CompareTag("Bullet"))
+        {
+            animator.SetBool("runFlag", true);
+        }
+    }
+}
