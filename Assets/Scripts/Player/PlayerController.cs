138c53cc471d974ff3d3fd6fb9c27b90830fd08a
BTHieu <20161498@student.hust.edu.vn>
2020-05-30 01:11:55 +0700

    Thêm crosshair, thêm model staff, chỉnh bắn góc nghiêng, thêm effect lúc bắn đạn

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,11 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     private Transform _playerParent;
     protected PlayerProperties _playerProperties;
+    private AudioSource _staffAudioSource;
+    private GameObject _staffVFX;
+    private Transform _bulletSpawnPoint;
 
     private void Start()
     {
         _playerParent = gameObject.transform;
         _playerProperties = gameObject.GetComponent<PlayerProperties>();
+        _bulletSpawnPoint = gameObject.transform.GetChild(1);
+        _staffAudioSource = gameObject.GetComponent<AudioSource>();
+        _staffVFX = gameObject.transform.GetChild(2).transform.GetChild(1).gameObject;
     }
 
     private void Update() 
@@ -24,8 +30,20 @@ public class PlayerController : MonoBehaviour
     public void SpawnBullet()
     {
         GameObject bullet;
-        bullet = BulletFactory.SpawnBullet(transform.position, transform.forward, _playerParent);
+        bullet = BulletFactory.SpawnBullet(_bulletSpawnPoint.position, _bulletSpawnPoint.forward, _playerParent);
         BulletMovement _bulletMovement = bullet.GetComponent<BulletMovement>();
-        _bulletMovement.ForwardShoot();
+        _bulletMovement.ForwardShoot(_bulletSpawnPoint.position, _bulletSpawnPoint.forward, _playerParent);
+
+        _staffAudioSource.Play();
+
+        _staffVFX.SetActive(true);
+        _staffVFX.GetComponent<ParticleSystem>().Play();
+        Invoke("StaffEffect", _staffVFX.GetComponent<ParticleSystem>().main.duration);
+    }
+
+    private void StaffEffect() 
+    {
+        _staffVFX.GetComponent<ParticleSystem>().Stop();
+        _staffVFX.SetActive(false);
     }
 }
