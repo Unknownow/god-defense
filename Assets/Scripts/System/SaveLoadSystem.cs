196111f89448d1b0d0dbb59f39f30bdae83d36df
Cường Bùi <cuong.bpt82@gmail.com>
2020-05-30 13:46:19 +0700

    thêm entity cho save và thêm SaveLoadSystem

---
+++ Assets/Scripts/System/SaveLoadSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class SaveLoadSystem : MonoBehaviour
+{
+    private static readonly string SavePath = Application.dataPath + "/Save/";
+    private static readonly string SaveFileName = "save.json";
+
+    public static void Save(Save save)
+    {
+        string saveData = JsonUtility.ToJson(save);
+        Utils.WriteFile(Path.Combine(SavePath, SaveFileName), saveData);
+    }
+
+    public static Save Load()
+    {
+        string saveData = Utils.ReadFile(SavePath + SaveFileName);
+        Save saveObject = JsonUtility.FromJson<Save>(saveData);
+        return saveObject;
+    }
+}
